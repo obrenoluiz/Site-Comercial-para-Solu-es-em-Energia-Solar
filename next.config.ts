a59aa456c83ed69a81c8de49756eb10c8a737f60
brenoluizdev <brenoluizpv3@gmail.com>
2025-01-27 14:17:17 -0300

    ajustes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Site-Comercial-para-Solucoes-em-Energia-Solar/' : '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
---
+++ src/components/Header/page.tsx
@@ -0,0 +1,86 @@
+'use client';
+
+import React, { useState } from 'react';
+import { FC } from 'react';
+import { FaPhone, FaWhatsapp, FaBars, FaTimes } from 'react-icons/fa';
+
+const Header: FC = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return (
+        <header className="bg-white text-dark-blue py-4 shadow-lg">
+            <div className="container mx-auto flex justify-between items-center px-4">
+            <div className="flex items-center space-x-4">
+                <img src="/placa_icon.png" alt="Logo" className="w-20 h-20 object-cover rounded-full" />
+                <div>
+                <h1 className="text-xl font-extrabold tracking-tight text-blue-900">
+                    ENERSOL
+                </h1>
+                <p className="text-sm font-medium text-gray-600">Energia Eficiente</p>
+                </div>
+            </div>
+
+            <div className="hidden md:flex space-x-8 items-center text-sm">
+                <div className="flex items-center space-x-2">
+                <FaPhone className="text-blue-700" />
+                <div>
+                    <p className="font-bold text-blue-900">Atendimento</p>
+                    <p className="text-gray-600">62 <span className="font-bold">00000-0000</span></p>
+                </div>
+                </div>
+                <div className="flex items-center space-x-2">
+                <FaWhatsapp className="text-green-500" />
+                <div>
+                    <p className="font-bold text-blue-900">Chamar no Whats</p>
+                    <p className="text-gray-600">62 <span className="font-bold">00000-0000</span></p>
+                </div>
+                </div>
+            </div>
+
+            <div className="hidden md:flex items-center space-x-4">
+                <button className="bg-blue-500 text-white text-sm font-bold py-2 px-4 rounded-full hover:bg-blue-700 transition duration-200">
+                ORÇAMENTO
+                </button>
+            </div>
+
+            <div className="md:hidden flex items-center">
+                <button onClick={toggleMenu} className="text-blue-900 text-xl cursor-pointer">
+                {isMenuOpen ? <FaTimes /> : <FaBars />}
+                </button>
+            </div>
+            </div>
+
+            {isMenuOpen && (
+            <div className="md:hidden bg-white shadow-lg">
+                <div className="flex flex-col items-center space-y-4 py-4">
+                <div className="flex flex-col items-center space-y-2">
+                    <div className="flex items-center space-x-2">
+                    <FaPhone className="text-blue-700" />
+                    <div>
+                        <p className="font-bold text-blue-900">Atendimento</p>
+                        <p className="text-gray-600">62 <span className="font-bold">00000-0000</span></p>
+                    </div>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                    <FaWhatsapp className="text-green-500" />
+                    <div>
+                        <p className="font-bold text-blue-900">Chamar no Whats</p>
+                        <p className="text-gray-600">62 <span className="font-bold">00000-0000</span></p>
+                    </div>
+                    </div>
+                </div>
+                <button className="bg-blue-500 text-white text-sm font-bold py-2 px-4 rounded-full hover:bg-blue-700 transition duration-200">
+                    ORÇAMENTO
+                </button>
+                </div>
+            </div>
+            )}
+        </header>
+    );
+};
+
+export default Header;
