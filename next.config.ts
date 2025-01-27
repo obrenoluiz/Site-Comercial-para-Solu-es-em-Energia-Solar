e229e5838123229ff0fdd2744cbf444252bec189
brenoluizdev <brenoluizpv3@gmail.com>
2025-01-27 14:49:08 -0300

    ajustes

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   output: "export",
   trailingSlash: true,
   reactStrictMode: true,
   images: {
-    domains: ['github.io', 'obrenoluiz.github.io'],
+      domains: ['github.io', 'obrenoluiz.github.io'],
   },
   devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-    buildActivityPosition: 'bottom-right',
+      appIsrStatus: false,
+      buildActivity: false,
+      buildActivityPosition: 'bottom-right',
   },
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import Header from "@/components/Header/page";
+import Header from "../components/Header/page";
+
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <main className="p-6">
-        <h2 className="text-3xl font-bold">Welcome to Solar Solutions</h2>
-        <p className="mt-4">Discover our innovative solar products and services!</p>
-      </main>
     </div>
   );
 }
